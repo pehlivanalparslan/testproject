202dbd738f67e1cd157044cbf2a279f74784949c
=Alparslan <=apehlivan4>
2021-02-14 14:58:37 +0300

    summed up values when displaying

--- testproject/src/testproject/SalesData.java
+++ testproject/src/testproject/SalesData.java
@@ -1,12 +1,19 @@
 package testproject;
-
+/**
+ * 
+ * Represents the given data for a given period
+ *
+ */
 public class SalesData {
 	int data[]= {0,4,42};
+	int sum;
 	
 	public void display() {
 		System.out.println("Data: ");
 		for (int i=0 ; i<data.length;i++) {
 			System.out.println("Next value: " + data[i]);
+			sum+=i;
 		}
+		System.out.println("Sum is: " + sum);
 	}
 }
