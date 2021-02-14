8f41d06f65b9da0ce8589063e1018bc3d94270fc
Alparslan <pehlivanalparslan@gmail.com>
2021-02-14 14:50:32 +0300

    change default values and refactor for loop

--- testproject/src/testproject/SalesData.java
+++ testproject/src/testproject/SalesData.java
@@ -1,11 +1,11 @@
 package testproject;
 
 public class SalesData {
-	int data[]= {0,4,42};
+	int data[]= {-1,0,4,42,84};
 	
 	public void display() {
 		System.out.println("Data: ");
-		for (int i=0 ; i<data.length;i++) {
+		for (int i:data) {
 			System.out.println("Next value: " + data[i]);
 		}
 	}
